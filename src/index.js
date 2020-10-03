9a93f80d934bfc29e6253711158f395199be2739
Su Guo <42634219+SaoriKaku@users.noreply.github.com>
2020-10-02 19:15:20 -0700

    Delete index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ItemEdit from './components/item-edit/item-edit.jsx';
-
-ReactDOM.render(<ItemEdit />, document.getElementById('root'));
-
