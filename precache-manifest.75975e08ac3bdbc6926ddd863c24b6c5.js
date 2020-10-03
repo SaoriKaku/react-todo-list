116db262465e6c7da9a496ec0e3938e6ae32bb0c
Su Guo <42634219+SaoriKaku@users.noreply.github.com>
2020-10-02 19:24:12 -0700

    2020-10-02 build update

---
+++ precache-manifest.75975e08ac3bdbc6926ddd863c24b6c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489c246ac8e27c9c3356fa79faf28688",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "2db164aed33d9e85a1df",
+    "url": "/react-todo-list/static/css/main.87956c34.chunk.css"
+  },
+  {
+    "revision": "a30716a7215031aaf136",
+    "url": "/react-todo-list/static/js/2.a4f5f06b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.a4f5f06b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db164aed33d9e85a1df",
+    "url": "/react-todo-list/static/js/main.c4cbc5ad.chunk.js"
+  },
+  {
+    "revision": "dc17a75b511fe1a54057",
+    "url": "/react-todo-list/static/js/runtime-main.9a686ea1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ac830896d9909b49404b80e1f2c9e8b.js"
+  "/react-todo-list/precache-manifest.75975e08ac3bdbc6926ddd863c24b6c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todo-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
